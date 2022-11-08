6c53dbfa79a93239339158fa6bd318687b13fec4
Fabp40 <104199929+Fabp40@users.noreply.github.com>
2022-11-08 17:07:10 +0100

    corrections esthétiques css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import App from './App'
 import './styles/index.scss'
-
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+
 const container = document.getElementById('root')
 const root = createRoot(container)
-
-
 root.render(<App />)
