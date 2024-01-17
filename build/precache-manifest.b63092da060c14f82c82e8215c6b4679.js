5430bd030958055be527bea77c0a6a8b8f8cefbd
Fabien <fabien.cdp@gmail.com>
2024-01-17 16:10:30 +0100

    redeploiement

---
+++ build/precache-manifest.b63092da060c14f82c82e8215c6b4679.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2a3bfc3e87656ed91aa058c8d1044c",
+    "url": "/Projet-Ka-7/index.html"
+  },
+  {
+    "revision": "84db8707a0949bd18976",
+    "url": "/Projet-Ka-7/static/css/main.7da4c8b2.chunk.css"
+  },
+  {
+    "revision": "56895019b44dc275d542",
+    "url": "/Projet-Ka-7/static/js/2.c6cde3b8.chunk.js"
+  },
+  {
+    "revision": "14f383ede202ef9c465726b230c728d0",
+    "url": "/Projet-Ka-7/static/js/2.c6cde3b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84db8707a0949bd18976",
+    "url": "/Projet-Ka-7/static/js/main.32eb2a48.chunk.js"
+  },
+  {
+    "revision": "3cde23fe80726060e792",
+    "url": "/Projet-Ka-7/static/js/runtime-main.cc715691.js"
+  },
+  {
+    "revision": "ed1410118e8bc1f8d259569133360f0d",
+    "url": "/Projet-Ka-7/static/media/down.ed141011.svg"
+  },
+  {
+    "revision": "4cd7959af15cf770c67506c0ca70a5a2",
+    "url": "/Projet-Ka-7/static/media/homeBackgroundSec1.4cd7959a.jpg"
+  },
+  {
+    "revision": "6b63b5917c6b2f452218a36fd515b56e",
+    "url": "/Projet-Ka-7/static/media/kalen-emsley-Bkci_8qcdvQ-unsplash.6b63b591.png"
+  },
+  {
+    "revision": "6b25a115f5ca4c954f09c86d4213a487",
+    "url": "/Projet-Ka-7/static/media/logo1.6b25a115.svg"
+  },
+  {
+    "revision": "d010b5f07b5ec8543094357a347ddb41",
+    "url": "/Projet-Ka-7/static/media/logo2.d010b5f0.svg"
+  },
+  {
+    "revision": "d22807e318ac745ea8316f623ac128a4",
+    "url": "/Projet-Ka-7/static/media/up.d22807e3.svg"
+  }
+]);
